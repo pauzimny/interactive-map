bc5ac02965d291672b38fb66c05f10e5c370ee81
Paulina Zimny <zimny.paulina@gmail.com>
2025-07-21 21:32:41 +0200

    Setup `MUI`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Grid from "@mui/material/Grid";
+import RocketLaunch from "@mui/icons-material/RocketLaunch";
 import "./App.css";
 
 function App() {
-  return <div>Interactive maps app</div>;
+  return (
+    <Grid container spacing={1}>
+      <Grid item>
+        <div>Interactive maps app</div>
+      </Grid>
+      <Grid item>
+        <RocketLaunch />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
